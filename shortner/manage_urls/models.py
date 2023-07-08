25ccdaaae82bf23b198c744994a85d08dd04745f
UTKARSH TIWARI <utkarshtiwari2803@gmail.com>
2023-07-08 15:14:11 +0530

    changin longurl length to cover for longer urls

--- shortner/manage_urls/models.py
+++ shortner/manage_urls/models.py
@@ -4,5 +4,5 @@ from django.db import models
 # Create your models here.
 class Urlinfo(models.Model):
     uuid = models.BigIntegerField(unique=True, primary_key=True)
-    longUrl = models.CharField(max_length=150, null=False)
+    longUrl = models.CharField(max_length=300, null=False)
     shortUrl = models.CharField(max_length=100, null=False)
